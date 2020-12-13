345c25d3e3c1709617b71d60eeaa7ff4744558d9
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2020-12-13 20:22:02 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-manifest",
+  ],
+};
